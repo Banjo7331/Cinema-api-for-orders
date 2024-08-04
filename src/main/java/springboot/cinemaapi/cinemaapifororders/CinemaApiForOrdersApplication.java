da57022f21a08983c32bff882e1afe7c8611db10
Banjo7331 <cylwikbartosz@gmail.com>
2024-08-04 20:48:54 +0200

    Initial commit

---
+++ src/main/java/springboot/cinemaapi/cinemaapifororders/CinemaApiForOrdersApplication.java
@@ -0,0 +1,13 @@
+package springboot.cinemaapi.cinemaapifororders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApiForOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApiForOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/cinemaapi/cinemaapifororders/CinemaApiForOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.cinemaapi.cinemaapifororders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiForOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
